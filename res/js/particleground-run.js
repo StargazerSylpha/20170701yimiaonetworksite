402c41edc2b3e4d124b74151b9b3514c52ef97a6
Sylpha <zhaolusylpha@gmail.com>
2017-08-07 20:04:18 +0800

    js文件补充
    网页js链接部分需要修改

---
+++ res/js/particleground-run.js
@@ -0,0 +1,18 @@
+
+/*particles run*/
+
+document.addEventListener(
+	'DOMContentLoaded', function ()
+ {
+  particleground(
+  	document.getElementById('page-header-lizibg'), 
+  	
+  {
+    dotColor: '#5cbdaa',
+    lineColor: '#5cbdaa'
+  }
+  );
+  
+}, 
+false
+);
